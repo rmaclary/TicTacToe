4bf2d6169ea80ac5dd90c4f61d3c47baf9554602
Rachel Maclary <79938920+rmaclary@users.noreply.github.com>
2021-04-02 21:12:55 -0400

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tictactoe;
 
 import java.util.Random;
@@ -6,7 +10,6 @@ public class Board
 {
     private int[][] board = new int[3][3];
     
-    //show a view of the board
     public void displayBoard()
     {
         System.out.println(" |0|1|2|");
@@ -15,26 +18,11 @@ public class Board
         System.out.println(printBoardRow(2));       
     }
     
-    private String printBoardRow(int row)
-    {
-        String output = row + "|";
-        for (int i=0; i<board[0].length; i++)
-        {
-            if(board[row][i] == 0) output += " ";
-            if(board[row][i] == 1) output += "X";
-            if(board[row][i] == 2) output += "O";
-            output += "|";
-        }
-        
-        return output;
-    }
-    
     public int[][] getBoard()
     {
         return board;
     }
     
-    //user makes play
     public boolean makePlay(int row, int col, String piece)
     {
         if(board[row][col] == 0) 
@@ -47,7 +35,6 @@ public class Board
         return false;
     }
     
-    //computer makes play in random open space
     public boolean makePlayComputer(String piece)
     {
         int row = new Random().nextInt(3);
@@ -70,7 +57,6 @@ public class Board
         return playMade;
     }
     
-    //test if the board is full
     public boolean fullBoard()
     {
         for(int row=0; row<board.length; row++)
@@ -82,12 +68,25 @@ public class Board
         }
         return true;
     }
-
+    
+    private String printBoardRow(int row)
+    {
+        String output = row + "|";
+        for (int i=0; i<board[0].length; i++)
+        {
+            if(board[row][i] == 0) output += " ";
+            if(board[row][i] == 1) output += "X";
+            if(board[row][i] == 2) output += "O";
+            output += "|";
+        }
+        
+        return output;
+    }
+    
     public enum Status 
     {
         PLAYING, XWIN, OWIN;
     }
-    //finds status of game (in progress/playing, won by either party)
     public Status findWinner()
     {
         for (int i = 0; i < 8; i++)
@@ -116,4 +115,5 @@ public class Board
         }
         return Status.PLAYING;
     }
+
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -7,19 +7,19 @@ public class TicTacToe {
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
-        Board board = new Board();
-        board.displayBoard();
+        Board board = new Board(); //create new empty board
+        board.displayBoard(); //show empty board
         System.out.println("Make your first move. Enter column: ");
         int col = Integer.parseInt(input.nextLine());
         System.out.println("Now enter row: ");
         int row = Integer.parseInt(input.nextLine());
-        board.makePlay(row, col, "X");
+        board.makePlay(row, col, "X"); //user plays using "X"
         boolean gameOver = false;
         
-        while(!board.fullBoard() && !gameOver)
+        while(!board.fullBoard() && !gameOver) //play continues while no winner and board isn't full
         {
-            board.makePlayComputer("O");
-            if (!board.findWinner().equals(board.findWinner().PLAYING))
+            board.makePlayComputer("O"); //computer plays with "O"
+            if (!board.findWinner().equals(board.findWinner().PLAYING)) //check for winner
             {
                 gameOver = true;
                 board.displayBoard();
@@ -30,8 +30,8 @@ public class TicTacToe {
             col = Integer.parseInt(input.nextLine());
             System.out.println("Now enter row: ");
             row = Integer.parseInt(input.nextLine());
-            board.makePlay(row, col, "X");
-            if (!board.findWinner().equals(board.findWinner().PLAYING))
+            board.makePlay(row, col, "X"); //user plays with "X"
+            if (!board.findWinner().equals(board.findWinner().PLAYING)) //check for winner
             {
                 gameOver = true;
             }
@@ -39,17 +39,17 @@ public class TicTacToe {
   
         }
         
-        if (board.findWinner().equals(board.findWinner().OWIN))
+        if (board.findWinner().equals(board.findWinner().OWIN)) //computer wins
         {
             System.out.println("Computer wins!");
         }
         else
         {
-            if (board.findWinner().equals(board.findWinner().XWIN))
+            if (board.findWinner().equals(board.findWinner().XWIN)) //user wins
             {
                 System.out.println("Congratulations, you win!");
             }
-            else 
+            else //no one wins
             {
                 System.out.println("It's a tie!");
             }
